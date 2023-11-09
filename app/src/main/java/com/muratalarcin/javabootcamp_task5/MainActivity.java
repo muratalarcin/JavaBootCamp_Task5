f9ff9f5dd2370da7c69cc9f0076cb4cc700bf539
Murat Alarcin <murattrbl@icloud.com>
2023-11-09 19:40:38 +0300

    main fonks started

--- app/src/main/java/com/muratalarcin/javabootcamp_task5/MainActivity.java
+++ app/src/main/java/com/muratalarcin/javabootcamp_task5/MainActivity.java
@@ -7,14 +7,30 @@ import android.os.Bundle;
 import com.muratalarcin.javabootcamp_task5.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
+    private StringBuilder currentNumber;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        currentNumber = new StringBuilder();
 
+        binding.button0.setOnClickListener(view -> appendNumber("0"));
+        binding.button1.setOnClickListener(view -> appendNumber("1"));
+        binding.button2.setOnClickListener(view -> appendNumber("2"));
+        binding.button3.setOnClickListener(view -> appendNumber("3"));
+        binding.button4.setOnClickListener(view -> appendNumber("4"));
+        binding.button5.setOnClickListener(view -> appendNumber("5"));
+        binding.button6.setOnClickListener(view -> appendNumber("6"));
+        binding.button7.setOnClickListener(view -> appendNumber("7"));
+        binding.button8.setOnClickListener(view -> appendNumber("8"));
+        binding.button9.setOnClickListener(view -> appendNumber("9"));
 
+    }
+    private void appendNumber(String number) {
+        currentNumber.append(number);
+        binding.textViewSonuc.setText(currentNumber.toString());
     }
 }
