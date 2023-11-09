d49cf6a96a7ac3996a05cb3615751712795d0015
Murat Alarcin <murattrbl@icloud.com>
2023-11-09 21:21:31 +0300

    upgrade

--- app/src/main/java/com/muratalarcin/javabootcamp_task5/MainActivity.java
+++ app/src/main/java/com/muratalarcin/javabootcamp_task5/MainActivity.java
@@ -1,13 +1,16 @@
 package com.muratalarcin.javabootcamp_task5;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.muratalarcin.javabootcamp_task5.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private StringBuilder currentNumber;
+    private StringBuilder sonuc;
     private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,22 +18,25 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        currentNumber = new StringBuilder();
+        sonuc = new StringBuilder();
 
-        binding.button0.setOnClickListener(view -> appendNumber("0"));
-        binding.button1.setOnClickListener(view -> appendNumber("1"));
-        binding.button2.setOnClickListener(view -> appendNumber("2"));
-        binding.button3.setOnClickListener(view -> appendNumber("3"));
-        binding.button4.setOnClickListener(view -> appendNumber("4"));
-        binding.button5.setOnClickListener(view -> appendNumber("5"));
-        binding.button6.setOnClickListener(view -> appendNumber("6"));
-        binding.button7.setOnClickListener(view -> appendNumber("7"));
-        binding.button8.setOnClickListener(view -> appendNumber("8"));
-        binding.button9.setOnClickListener(view -> appendNumber("9"));
+
+        binding.button0.setOnClickListener(view -> sayiEkleme("0"));
+        binding.button1.setOnClickListener(view -> sayiEkleme("1"));
+        binding.button2.setOnClickListener(view -> sayiEkleme("2"));
+        binding.button3.setOnClickListener(view -> sayiEkleme("3"));
+        binding.button4.setOnClickListener(view -> sayiEkleme("4"));
+        binding.button5.setOnClickListener(view -> sayiEkleme("5"));
+        binding.button6.setOnClickListener(view -> sayiEkleme("6"));
+        binding.button7.setOnClickListener(view -> sayiEkleme("7"));
+        binding.button8.setOnClickListener(view -> sayiEkleme("8"));
+        binding.button9.setOnClickListener(view -> sayiEkleme("9"));
+        binding.buttonToplama.setOnClickListener(view -> sayiEkleme(" + "));
+
 
     }
-    private void appendNumber(String number) {
-        currentNumber.append(number);
-        binding.textViewSonuc.setText(currentNumber.toString());
+    private void sayiEkleme(String sayi) {
+        sonuc.append(sayi);
+        binding.textViewSonuc.setText(sonuc.toString());
     }
 }
